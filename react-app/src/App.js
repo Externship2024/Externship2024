6921e019009c81b9d9d8b7d0b2a31351bc2cc938
mai-tran-03 <103965617+mai-tran-03@users.noreply.github.com>
2024-12-12 14:17:56 -0800

    Create react-app (#14)
    * Create react-app
    
    * Add some navigation components to the front page

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+// import ButtonGroup from "./components/ButtonGroup";
+// import NavButton from "./components/NavButton";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      {/* <ButtonGroup /> */}
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
