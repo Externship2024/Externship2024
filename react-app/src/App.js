9eb7ef178f552b5cb62a0e31e747da823255995b
Mai Tran <mai.tran1714@gmail.com>
2024-12-13 12:48:56 -0800

    Add two column layout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container, Row, Col, Modal } from 'reactstrap';
 import Home from './components/Home';
-// import TwoColLayout from './components/TwoColLayout';
+import TwoColLayout from './components/TwoColLayout';
 
 function App() {
   return (
     <>
       <Home />
-      {/* <Container>
+      <Container>
         <Row className="mt-4">
           <Col>
             <h1>Two Column Layout</h1>
             <TwoColLayout />
           </Col>
         </Row>
-      </Container> */}
+      </Container>
     </>
   );
 }
---
+++ react-app/src/components/TwoColLayout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Row, Col, Card, CardBody, CardTitle, CardText } from 'reactstrap';
+
+function TwoColLayout() {
+    return (
+        <Container>
+            <Row md="4" sm="2" xs="1">
+                <Col md="6">
+                    <Card>
+                        <CardBody>
+                            <CardText>
+                                Contact info: 123-456-7890 <br />
+                                Departure time: 12:00 PM <br />
+                                From: Sayles <br />
+                                To: Tin Tea <br />
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="6">
+                    <Card>
+                        <CardBody>
+                            <CardText>
+                                Contact info: @user_name <br />
+                                Departure time: 3:00 PM <br />
+                                From: James <br />
+                                To: Asian Market <br />
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container >
+    );
+}
+
+export default TwoColLayout;
