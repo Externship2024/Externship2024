915b6da66cd4e0ca82b855bea4fc7b41aa501e18
mai-tran-03 <103965617+mai-tran-03@users.noreply.github.com>
2024-12-13 15:12:22 -0800

    Allow requested ride info shown/hidden by button (#21)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,20 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container, Row, Col, Modal } from 'reactstrap';
 import Home from './components/Home';
-import TwoColLayout from './components/TwoColLayout';
 
 function App() {
   return (
     <>
       <Home />
-      <Container>
-        <Row className="mt-4">
-          <Col>
-            <h1>Two Column Layout</h1>
-            <TwoColLayout />
-          </Col>
-        </Row>
-      </Container>
     </>
   );
 }
