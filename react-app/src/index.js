5c85158645c981843e2ca46c756c47b024b3c12e
mai-tran-03 <103965617+mai-tran-03@users.noreply.github.com>
2024-12-12 22:00:52 -0800

    Try a different react bootstrap to create components (#18)

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
 // Bootstrap CSS
-// import "bootstrap/dist/css/bootstrap.css";
-// // Bootstrap Bundle JS
+// import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
